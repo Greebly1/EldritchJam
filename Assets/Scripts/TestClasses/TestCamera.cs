21524e619f7bc252a9f141bba7b3caad1b3305d4
Collin Strauch <54947383+Collide01@users.noreply.github.com>
2024-06-01 19:42:07 -0700

    Camera movement finalized

--- Assets/Scripts/TestClasses/TestCamera.cs
+++ Assets/Scripts/TestClasses/TestCamera.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.InteropServices;
 
 public class TestCamera : MonoBehaviour
 {
     private float screenWidth;
     private float screenHeight;
+    private Vector2 prevMousePosition;
+
+    [DllImport("user32.dll")]
+    static extern bool SetCursorPos(float X, float Y);
+
     public float sensitivity;
+    [SerializeField] private GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
@@ -20,9 +27,17 @@ public class TestCamera : MonoBehaviour
     {
         if (Input.GetMouseButton(1)) // Right mouse button
         {
+            Cursor.lockState = CursorLockMode.Locked;
             float mousePositionDeltaX = -Input.mousePositionDelta.x * 0.001f * sensitivity;
             float mousePositionDeltaY = -Input.mousePositionDelta.y * 0.001f * sensitivity;
             Camera.main.transform.position += new Vector3(mousePositionDeltaX, mousePositionDeltaY, 0);
+            Camera.main.transform.position = new Vector3(Mathf.Clamp(Camera.main.transform.position.x, -gameManager.currentMap.GetComponent<SpriteRenderer>().bounds.size.x / 2 + screenWidth, gameManager.currentMap.GetComponent<SpriteRenderer>().bounds.size.x / 2 - screenWidth),
+                Mathf.Clamp(Camera.main.transform.position.y, -gameManager.currentMap.GetComponent<SpriteRenderer>().bounds.size.y / 2 + screenHeight, gameManager.currentMap.GetComponent<SpriteRenderer>().bounds.size.y / 2 - screenHeight),
+                -10);
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
         }
     }
 }
